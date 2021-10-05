5c39f3d056f526d61459e85e2075470d448ab2d0
dsa <veretino1in7@bk.ru>
2021-10-05 08:31:19 +0500

    commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,133 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ 'test' }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12 border-gray-200">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="my_block">
+                    <div class="mx-auto w-3/4 my-4">
+        <ul class="grid grid-cols-2 gap-4 list-none">
+            <li>
+                <div class="shadow-sm bg-white rounded-lg h-20">1</div>
+            </li>
+            <li>
+                <div class="shadow bg-white rounded-lg h-20">2</div>
+            </li>
+            <li>
+                <div class="shadow-md bg-white rounded-lg h-20">3</div>
+            </li>
+            <li>
+                <div class="shadow-lg bg-white rounded-lg h-20">4</div>
+            </li>
+            <li>
+                <div class="shadow-xl bg-white rounded-lg h-20">5</div>
+            </li>
+            <li>
+                <div class="shadow-2xl bg-white rounded-lg h-20">6</div>
+            </li>
+        </ul>
+
+
+        <ul class="space-y-2 my-20 bg-gray-400 p-3 rounded">
+        <li>
+            <ul class="grid grid-cols-10 h-7">
+            <li class="bg-red-50"></li>
+            <li class="bg-red-100"></li>
+            <li class="bg-red-200"></li>
+            <li class="bg-red-300"></li>
+            <li class="bg-red-400"></li>
+            <li class="bg-red-500"></li>
+            <li class="bg-red-600"></li>
+            <li class="bg-red-700"></li>
+            <li class="bg-red-800"></li>
+            <li class="bg-red-900"></li>
+            </ul>
+        </li>
+        <li>
+            <ul class="grid grid-cols-10 h-7">
+            <li class="bg-yellow-50"></li>
+            <li class="bg-yellow-100"></li>
+            <li class="bg-yellow-200"></li>
+            <li class="bg-yellow-300"></li>
+            <li class="bg-yellow-400"></li>
+            <li class="bg-yellow-500"></li>
+            <li class="bg-yellow-600"></li>
+            <li class="bg-yellow-700"></li>
+            <li class="bg-yellow-800"></li>
+            <li class="bg-yellow-900"></li>
+            </ul>
+        </li>
+        <li>
+            <ul class="grid grid-cols-10 h-7">
+            <li class="bg-green-50"></li>
+            <li class="bg-green-100"></li>
+            <li class="bg-green-200"></li>
+            <li class="bg-green-300"></li>
+            <li class="bg-green-400"></li>
+            <li class="bg-green-500"></li>
+            <li class="bg-green-600"></li>
+            <li class="bg-green-700"></li>
+            <li class="bg-green-800"></li>
+            <li class="bg-green-900"></li>
+            </ul>
+        </li>
+        <li>
+            <ul class="grid grid-cols-10 h-7">
+            <li class="bg-blue-50"></li>
+            <li class="bg-blue-100"></li>
+            <li class="bg-blue-200"></li>
+            <li class="bg-blue-300"></li>
+            <li class="bg-blue-400"></li>
+            <li class="bg-blue-500"></li>
+            <li class="bg-blue-600"></li>
+            <li class="bg-blue-700"></li>
+            <li class="bg-blue-800"></li>
+            <li class="bg-blue-900"></li>
+            </ul>
+        </li>
+        <li>
+            <ul class="grid grid-cols-10 h-7">
+            <li class="bg-purple-50"></li>
+            <li class="bg-purple-100"></li>
+            <li class="bg-purple-200"></li>
+            <li class="bg-purple-300"></li>
+            <li class="bg-purple-400"></li>
+            <li class="bg-purple-500"></li>
+            <li class="bg-purple-600"></li>
+            <li class="bg-purple-700"></li>
+            <li class="bg-purple-800"></li>
+            <li class="bg-purple-900"></li>
+            </ul>
+        </li>
+        <li>
+            <ul class="grid grid-cols-10 h-7">
+            <li class="bg-pink-50"></li>
+            <li class="bg-pink-100"></li>
+            <li class="bg-pink-200"></li>
+            <li class="bg-pink-300"></li>
+            <li class="bg-pink-400"></li>
+            <li class="bg-pink-500"></li>
+            <li class="bg-pink-600"></li>
+            <li class="bg-pink-700"></li>
+            <li class="bg-pink-800"></li>
+            <li class="bg-pink-900"></li>
+            </ul>
+        </li>
+        </ul>
+</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+
+
+</x-app-layout>
+
+
+            
